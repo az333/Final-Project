a4e85fbeb41d66874b677bfa10568b59b56d0e29
az333 <azverovich@stuy.edu>
2016-12-29 21:11:34 -0500

    Finished sumSquare

--- Sudoku.java
+++ Sudoku.java
@@ -33,11 +33,11 @@ public class Sudoku {
     }
 
 	
-    /**Set all values on the board to underscores'_'*/
+    //Set all values on the board to 10
     private void clear(){		
 	for (int row = 0; row < board.length; row ++) {
 	    for ( int column = 0; column < board[row].length; column ++) {
-		board[row][column] = '_';
+		board[row][column] = 10;
 	    }
 	}
     }
@@ -45,29 +45,69 @@ public class Sudoku {
     //max sum is 45 
    
     public void addNumber (int row, int col) {
-     
+	int diffrow = 45 - sumRow(row);
+	int diffcol = 45 - sumCol(col);
+	int diffsquare = 45 - sumSquare(row,col);
+	int maxint  = (diffsquare < Math.min(diffrow, diffcol)) ? diffsquare: Math.min (diffrow, diffcol);
+	// if (maxint > 0) {
+	    //int nextint = randgen.nextInt(maxint + 1); 
+	//	}
+       
+	    }
+
+    private int sumRow (int r) {
+	return sumRow (r, board[r].length);
     }
 
-    public int sumRow (int r) {
+    //non inclusive of endcol
+    private  int sumRow (int r, int endcol) {
 	int sum = 0;
 	int[] row = board[r];
-	for (int index = 0; index < row.length; index ++) {
-	    sum += row[index]; 
+	for (int index = 0; index < endcol; index ++) {
+	    if (row[index] != 10) {
+		sum += row[index];
+	    }
 	}
 
 	return sum;
     }
 
-    public int sumCol (int col) {
+    private int sumCol (int col) {
+	return sumCol (col, board.length );
+    }
+
+    //noninclusive of endrow
+    private int sumCol (int col, int endrow) {
 	int sum = 0;
-	for (int row = 0; row < board.length; row ++) {
-	    sum += board[row][col];
+	for (int row = 0; row < endrow; row ++) {
+	    if (board[row][col] != 10) {
+		sum += board[row][col];
+	    }
 	} 
 	return sum;
     }
 
-    public int sumSquare (int row, int col) {
+    private int findSquare (int row, int col) {
+	int square;
+	if (row  < 3) {
+	    square = ( col < 3) ? 1 : ( (col > 5) ?  3: 2);	    
+	}else if (row > 5) {
+	    square = ( col < 3) ? 7 : ( (col > 5) ?  9: 8);
+	}else {
+	    square = ( col < 3) ? 4 : ( (col > 5) ?  6: 5);
+	}
+	return square;
+    }
+
+
+    private int sumSquare (int row, int col) {
 	int sum = 0;
+	int square = findSquare ( row,  col);
+	int firstrow = (row < 3) ? 0 : ( (row > 5) ? 6: 3); 
+	int firstcol = (col < 3) ? 0 : ( (col > 5) ? 6: 3);
+	for (int r = firstrow; r < firstrow + 3; r ++) {
+	    sum += sumRow(r, firstcol + 3);
+	}		   
 	return sum;
     }
 
@@ -79,7 +119,25 @@ public class Sudoku {
 	}
     }
 
+    public String toString () {
+	String str = "";
+	for (int r = 0 ; r < board.length; r ++) {
+	    for (int c = 0; c < board[r].length; c ++) {
+		str = str +  board[r][c] + " ";
+	    }
+	    str += "\n";
+	}
+	return str; 
+    }
 
-
+    public static void main (String[] args) {
+	Sudoku a = new Sudoku ();
+	System.out.println (a);
+	System.out.println (a.sumRow(1));
+	System.out.println (a.sumCol(1));
+	System.out.println (a.sumSquare(1,4));
+	
+	
+    } 
 
     }
