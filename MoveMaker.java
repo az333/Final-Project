6c053798b65cbc29528a07055b54d190e7ac90d1
Donia Tung <doniatung32@gmail.com>
2017-01-09 12:31:47 -0500

    I have a plan now

--- MoveMaker.java
+++ MoveMaker.java
@@ -5,14 +5,14 @@ import java.awt.event.*;
 
 
 
-public class MoveListener implements ActionListener{
+public class MoveMaker implements MouseListener{
 
-
-
-    public void actionPerformed(ActionEvent e){
+    public void mouseLocation(MouseEvent e){
+	//I need to use getXOnScreen() and getYOnScreen and then take boardPixels of the grid and divide that by 9 for rows and by 9 for columns to figure out which box the mouse is in. 
 
 
     }
+
 
 
     
