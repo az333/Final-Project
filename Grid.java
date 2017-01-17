9de604c9c0f201dcca6e1ac43d07f7d2e70ba1fd
Angelica Zverovich <azverovich@stuy.edu>
2017-01-17 00:00:19 -0500

    final commit

---
+++ Grid.java
@@ -0,0 +1,99 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Grid extends JComponent {
+
+    private static int boxPixels = 50;
+    private static int puzzleSize = 9;
+    private static int subSquare = 3;
+    private static int boardPixels = boxPixels * puzzleSize;
+    private static int textOffset = 15;
+    private static Font textFont = new Font ("Sansserif", Font.BOLD, 24);
+
+
+
+    private Sudoku initialBoard;
+    private Sudoku solved;
+    
+
+    public Grid(Sudoku initialBoard) {
+        setPreferredSize(new Dimension(boardPixels + 5, boardPixels + 5));
+        setBackground(Color.WHITE);
+        this.initialBoard = initialBoard;
+	solved = new Sudoku(initialBoard.getBoard());
+	SudokuSolver.solveSudoku(solved);
+	
+
+    }
+    
+
+    @Override public void paintComponent(Graphics g) {
+        //... Draw background.
+        g.setColor(getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.BLACK);        
+        drawGridLines(g);
+        drawCellValues(g);
+    }
+
+    private void drawGridLines(Graphics g) {
+	//this draws the grid by making lines every 50 pixels
+        for (int i = 0; i <= puzzleSize; i++) {
+            int acrossOrDown = i * boxPixels;
+            g.drawLine(acrossOrDown, 0, acrossOrDown, boardPixels);
+            g.drawLine(0, acrossOrDown, boardPixels, acrossOrDown);
+             if (i % subSquare == 0) {
+                acrossOrDown++; 
+                g.drawLine(acrossOrDown, 0, acrossOrDown, boardPixels);
+                g.drawLine(0, acrossOrDown, boardPixels, acrossOrDown);
+            }
+        }
+    }
+
+
+    public void setFontColor(Font f){
+	textFont = f;
+
+    }
+
+    
+    private void drawCellValues(Graphics g) {
+        g.setFont(textFont);
+        for (int i = 0; i < puzzleSize; i++) {
+            int yDisplacement = (i+1) * boxPixels - textOffset;
+            for (int j = 0; j < puzzleSize; j++) {
+		
+		if (initialBoard.ogNum(i,j) == 10){
+		    g.setFont(new Font ("Sansserif", Font.PLAIN, 24));
+		    //System.out.println ("solution " + solved);
+		    //System.out.println ("initial " + initialBoard);
+		    if (solved.getNum(i,j) != initialBoard.getNum(i,j) && initialBoard.getCheck()){
+			//System.out.println ("row " + i + " col " + j);
+			g.setColor(Color.RED);
+			g.setFont(textFont);
+			initialBoard.setRed(true); 
+		    }
+		    if (solved.getNum(i,j) == initialBoard.getNum(i,j)) {
+			g.setColor(Color.BLACK);
+		    } 
+		    //this.setFontColor(g);
+		}
+		if (initialBoard.ogNum(i,j) != 10){
+		    g.setFont(textFont);
+		    g.setColor(Color.BLACK);
+		}
+                if (initialBoard.getNum(i, j) > 0 && initialBoard.getNum(i, j) < 10) {
+                    int xDisplacement = j * boxPixels + textOffset;
+                    g.drawString("" + initialBoard.getNum(i,j), xDisplacement, yDisplacement);
+		}
+		else{
+		    int xDisplacement = j * boxPixels + textOffset;
+		    g.drawString("", xDisplacement, yDisplacement);
+		}
+	    }
+	}
+    }
+}
